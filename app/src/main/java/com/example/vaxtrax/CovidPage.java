49852b4703d3b8e89c60f070fbe02aad2e486b61
UNSWShaamir <shaamirnehal@gmail.com>
2020-06-25 17:06:46 +1000

    Added vaccine and covid pages

---
+++ app/src/main/java/com/example/vaxtrax/CovidPage.java
@@ -0,0 +1,14 @@
+package com.example.vaxtrax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CovidPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid_page);
+    }
+}
---
+++ app/src/main/java/com/example/vaxtrax/VaccinePage.java
@@ -0,0 +1,14 @@
+package com.example.vaxtrax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VaccinePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vaccine_page);
+    }
+}
