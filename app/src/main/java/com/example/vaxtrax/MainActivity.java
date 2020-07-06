e3d3b05c3a40e83f9aa172cce246026e089172aa
UNSWShaamir <shaamirnehal@gmail.com>
2020-07-06 21:19:44 +1000

    Added xml for vaccine page recycler view and row. Added intents for landing page buttons.

--- app/src/main/java/com/example/vaxtrax/MainActivity.java
+++ app/src/main/java/com/example/vaxtrax/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.vaxtrax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button covidBtn;
+    Button vaccineBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        covidBtn = findViewById(R.id.covidBtn);
+        vaccineBtn = findViewById(R.id.vaccineBtn);
+
+        covidBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CovidPage.class);
+                startActivity(intent);
+            }
+        });
+
+        vaccineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VaccinePage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
