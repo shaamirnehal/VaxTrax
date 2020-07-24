a87de8432a4c183ef1f0e92a7ae55a8ccca13e58
UNSWShaamir <shaamirnehal@gmail.com>
2020-07-24 20:11:15 +1000

    Integrated python API and added to vaccine recycler view

--- app/src/main/java/com/example/vaxtrax/RVadapter.java
+++ app/src/main/java/com/example/vaxtrax/RVadapter.java
@@ -1,10 +1,10 @@
 package com.example.vaxtrax;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -41,6 +41,7 @@ public class RVadapter extends RecyclerView.Adapter<RVadapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull RVadapter.ViewHolder holder, int position) {
         Vaccines dataObj = data.get(position);
+        Log.i("onBindViewHolder", "onBindViewHolder: " + dataObj.getInfo());
         holder.nametv.setText(dataObj.getName());
         holder.stagetv.setText(dataObj.getStage());
         holder.typetv.setText(dataObj.getType());
@@ -56,9 +57,6 @@ public class RVadapter extends RecyclerView.Adapter<RVadapter.ViewHolder> {
         TextView nametv;
         TextView stagetv;
         TextView typetv;
-        ImageView ivDisplay;
-
-        //for handling views
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
--- app/src/main/java/com/example/vaxtrax/VaccinePage.java
+++ app/src/main/java/com/example/vaxtrax/VaccinePage.java
@@ -4,9 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -15,24 +29,57 @@ public class VaccinePage extends AppCompatActivity implements RVadapter.ItemClic
     RecyclerView rv;
     RVadapter adapter;
     ArrayList<Vaccines> data = new ArrayList<>();
+    RequestQueue reqQ;
+    String baseUrl = "http://10.0.2.2:8080";
+    String listVac = "/vaccine";
+    ArrayList<Vaccines> vList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaccine_page);
-        Vaccines vac1 = new Vaccines("Gilead Sciences","DNA","Pre-clinical",
-                "Loremipsum asjdasndjasndjasnd");
-
-        data.add(vac1);
         rv = findViewById(R.id.rv);
         rv.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new RVadapter(data, this);
+        vList = new ArrayList<>();
+        adapter = new RVadapter(vList, getApplicationContext());
+
+        reqQ = Volley.newRequestQueue(this);
+        StringRequest req = new StringRequest(Request.Method.GET, baseUrl + listVac,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONArray vaccinesArray = new JSONArray(response);
+                            for (int i = 0; i < vaccinesArray.length(); i++)    {
+                                JSONObject vaccines = vaccinesArray.getJSONObject(i);
+                                String name = vaccines.getString("name");
+                                String type = vaccines.getString("type");
+                                String stage = vaccines.getString("stage");
+                                String info = vaccines.getString("info");
+                                Vaccines vac = new Vaccines(name, type, stage, info);
+                                vList.add(vac);
+                            }
+                            rv.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            Log.i("TAG", "onResponse: " + e.getMessage());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("TAG", "onErrorResponse: " + error.getMessage());
+            }
+        });
+        reqQ.add(req);
         adapter.setClickListener(this);
-        rv.setAdapter(adapter);
     }
 
     @Override
     public void onItemClick(View view, int position) {
-
+        Intent intent = new Intent(this, vaccinesDetail.class);
+        startActivity(intent);
     }
 }
